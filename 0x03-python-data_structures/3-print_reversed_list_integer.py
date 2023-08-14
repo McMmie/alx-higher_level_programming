ec45dde499881854b43ba67058ffa0e3287d4288
McMmie <tharausamuel01@gmail.com>
2023-08-14 23:50:28 +0000

    matrix fixed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
+    str = "{:d}"
     for x in my_list:
-        print("{:d}".format(x))
+        print(str.format(x))
