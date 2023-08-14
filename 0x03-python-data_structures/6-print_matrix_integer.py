b7ffb19e53089cd79d75fd249c92ce0d4e9ff32b
McMmie <tharausamuel01@gmail.com>
2023-08-14 23:47:28 +0000

    matrix fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    str = "{:d}"
     for rows in matrix:
-        for elements in rows:
-            print("{:d}".format(elements), end=" ")
+        for idx, elements in enumerate(rows):
+
+            print(str.format(elements), end="")
+            if idx < len(rows) - 1:
+                print(" ", end="")
         print()
