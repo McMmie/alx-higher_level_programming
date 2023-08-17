d8bdd30858f03328be1f3de16992255f5b848819
McMmie <tharausamuel01@gmail.com>
2023-08-17 00:32:59 +0000

    advanced squaring

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x ** 2, row))for row in matrix]
+    return [list(map(lambda a: (list(map(lambda b: b ** 2, a))), matrix))]
+
+
+""" alternatively:
+    return [list(map(lambda x: x**2, row))for row in matrix]
+    """
