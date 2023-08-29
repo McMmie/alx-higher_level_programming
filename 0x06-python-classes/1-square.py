f2a0c283283f6eab9f7dbcde19ca5c3aa391d7d0
McMmie <tharausamuel01@gmail.com>
2023-08-29 20:43:30 +0000

    private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" It creates a class that defines a
+square and creates a
+private instance attribute"""
+
+
+class Square:
+    def __init__(self, size):
+        self._size = size
