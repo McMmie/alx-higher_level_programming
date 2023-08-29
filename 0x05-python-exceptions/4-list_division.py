1c554b6df0a14fe71b2cfb30d25e590da16a5731
McMmie <tharausamuel01@gmail.com>
2023-08-29 00:59:15 +0000

    handled more errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,7 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             new_list.append(0)
             print("division by 0")
-        except TypeError:
+        except (ValueError, TypeError):
             print("wrong type")
             print("out of range")
             new_list.append(0)
