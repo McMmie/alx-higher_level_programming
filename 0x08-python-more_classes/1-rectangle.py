d8dbe331693cb4e48701a07234734556558c6680
McMmie <tharausamuel01@gmail.com>
2023-09-04 21:42:40 +0000

    fixed indentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,46 +23,46 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """ It retrieves the private instance attribute
+    @property
+    def width(self):
+        """ It retrieves the private instance attribute
             return:
                 An attribute
-            """
-            return self.__width
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """
+    @width.setter
+    def width(self, value):
+        """
             Setter
             Args:
                 value(int): an integer
-            """
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
-        @property
-        def height(self):
-            """ It retrieves the private instance attribute
+    @property
+    def height(self):
+        """ It retrieves the private instance attribute
             return:
                 An attribute
-            """
-            return self.__height
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """
+    @height.setter
+    def height(self, value):
+        """
             Setter
             Args:
                 value(int): an integer
-            """
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value 
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
