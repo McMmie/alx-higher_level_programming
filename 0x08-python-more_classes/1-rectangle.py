1c821632638411740ec903fc1e146b3bc61dc338
McMmie <tharausamuel01@gmail.com>
2023-09-04 21:02:44 +0000

    defined the class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+""" It creates a class that defines a
+rectangle and creates a
+private instance attribute"""
+
+
+class Rectangle:
+    """A class that defines a private
+    instance attribute.
+    Args:
+        width: width of a rectangle
+        height: height of a rectangle
+        """
+
+    def __init__(self, width=0, height=0):
+        """This is the docstring for the __init__
+        method.
+        Args:
+            width(int): width of the rectangle
+            height(int): height of the rectangle
+            """
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            """ It retrieves the private instance attribute
+            return:
+                An attribute
+            """
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """
+            Setter
+            Args:
+                value(int): an integer
+            """
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        @property
+        def height(self):
+            """ It retrieves the private instance attribute
+            return:
+                An attribute
+            """
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """
+            Setter
+            Args:
+                value(int): an integer
+            """
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value 
