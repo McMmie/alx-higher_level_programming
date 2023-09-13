bea7d166219f12783d15127c8666b6387beb5b68
McMmie <tharausamuel01@gmail.com>
2023-09-13 06:35:41 +0000

    pascals triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+an interview technichal
+"""
+import math
+
+
+def pascal_triangle(n):
+    """
+    returns a ist of lists
+    """
+
+    triangle = []
+    my_list = [1]
+
+    if n <= 0:
+        return
+    else:
+        for i in range(n):
+            if i != 0:
+                row = triangle[-1]
+                my_list = [1]
+                for j in range(1, i):
+                    my_list.append(row[j - 1] + row[j])
+                my_list.append(1)
+
+            triangle.append(my_list)
+
+    return triangle
