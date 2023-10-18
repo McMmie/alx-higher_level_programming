5262f203e14603bad1f0d3e572fdef86e47d0afc
McMmie <tharausamuel01@gmail.com>
2023-10-18 21:10:26 +0000

    not null constraint

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- This script creates a database(hbtn_0d_2) and a user(user_0d_2)
+-- The password is set to user_0d_2_pwd
+-- The script should not fail if script fails
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
