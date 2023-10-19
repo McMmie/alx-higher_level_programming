21a6adcc5c3504ac2c018f5ae98a42ce79e23fb0
McMmie <tharausamuel01@gmail.com>
2023-10-19 00:20:27 +0000

    imported database

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+--a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+
+    SELECT tv_shows.title, tv_show_genres.genre_id
+      FROM tv_shows
+INNER JOIN tv_show_genres
+     WHERE tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
