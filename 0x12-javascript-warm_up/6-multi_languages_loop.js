59f422760053ef258649c01e0184f20ea7b17efa
McMmie <tharausamuel01@gmail.com>
2023-11-13 20:57:38 +0000

    semistandard style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 let i = 0;
-const myArray = ['C is fun',
-		 'Python is cool',
-		 'JavaScript is amazing',];
+const myArray = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'];
 
 while (myArray[i]) {
-  console.log(myArray[i++])
+  console.log(myArray[i++]);
 }
