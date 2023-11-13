997d10098bc53d6718f27b34cdd12cb6a9d95e7d
McMmie <tharausamuel01@gmail.com>
2023-11-13 17:58:22 +0000

    using command line arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// prints command line arguments
+const argv = process.argv;
+
+if (argv.length < 3) {
+	console.log('No argument');
+}else if (argv.length === 3) {
+	console.log('Argument found');
+}else {
+	console.log('Arguments found');
+}
