792a159e84baed0c866021c995d56c6978426e7e
McMmie <tharausamuel01@gmail.com>
2023-11-13 20:56:47 +0000

    added isnan function

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!usr/bin/node
 const argv = process.argv;
-let num = parseInt(parseFloat(argv[2]));
-if (!num || num === NaN) {
+const num = parseInt(parseFloat(argv[2]));
+if (!num || num.isnan()) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + num);
