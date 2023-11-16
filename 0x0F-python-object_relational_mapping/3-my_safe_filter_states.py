8cd020939a92b2d0dcf589c24a462d6d8237459d
McMmie <tharausamuel01@gmail.com>
2023-11-16 23:56:27 +0000

    added permission

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is a module for mysqldb
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    argv = sys.argv
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    state_name_searched = argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * from states WHERE name = %s ORDER BY states.id ASC", (state_name_searched,))
+    states = cur.fetchall()
+    for rows in states:
+        print(rows)
+    cur.close()
+    db.close()
