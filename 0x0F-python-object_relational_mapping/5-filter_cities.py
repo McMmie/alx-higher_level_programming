c25365ca5b1f68c09774a0825aefd4c79fa6b056
McMmie <tharausamuel01@gmail.com>
2023-11-17 00:45:54 +0000

    filtering by city name

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,18 +8,18 @@ import sys
 
 if __name__ == '__main__':
     argv = sys.argv
-    if (len(argv) != 4):
+    if (len(argv) != 5):
+        print("Usage: Arguments must be four!")
         sys.exit(1)
 
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    state_name = argv[4]
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name\
-                 FROM cities\
-                 JOIN states\
-                 WHERE cities.state_id = states.id\
-                 ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.name\
+                   FROM cities\
+                  WHERE cities.state_id = %s.id\
+                  ORDER BY cities.id ASC", (state_name,))
     cities = cur.fetchall()
-    for rows in cities:
-        print(rows)
+    print(cities)
     cur.close()
     db.close()
