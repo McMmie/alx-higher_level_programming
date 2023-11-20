89e1bd2b29bd105187e55127ed5a13e8638a364c
McMmie <tharausamuel01@gmail.com>
2023-11-20 00:43:58 +0000

    fixed import issue

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,13 @@ import MySQLdb
 import sys
 
 
-argv = sys.argv
-db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-cur = db.cursor()
-cur.execute("SELECT * from states")
-states = cur.fetchall()
-for rows in states:
-    print(rows)
-cur.close()
-db.close()
+if __name__ == '__main__':
+    argv = sys.argv
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * from states")
+    states = cur.fetchall()
+    for rows in states:
+        print(rows)
+    cur.close()
+    db.close()
