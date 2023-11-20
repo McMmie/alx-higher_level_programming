0fbb9d9beec745752fd083758f7b7e9126dfa280
McMmie <tharausamuel01@gmail.com>
2023-11-20 01:17:40 +0000

    new changes

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,12 +12,16 @@ if __name__ == '__main__':
     if len(sys.argv) != 5:
         sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (sys.argv[1], sys.argv[2],
+                            sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     target = sys.argv[4]
-    state = session.query(State).filter(State.name.like(f'%{target}%')).order_by(State.id).all()
+    state = session.query(State).filter(
+            State.name.like(f'%{target}%')
+            ).order_by(State.id).all()
     if state:
         for i in state:
             print(i.id)
