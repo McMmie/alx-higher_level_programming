15aad21627b7db13dded05d1af719c5cee58b3e4
McMmie <tharausamuel01@gmail.com>
2023-11-20 01:24:07 +0000

    refactored code

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,9 @@ if __name__ == '__main__':
     if len(sys.argv) != 4:
         sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (sys.argv[1], sys.argv[2],
+                            sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     session.query(State).filter(State.name.like("%a%")).delete()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,11 +10,15 @@ import sys
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (sys.argv[1], sys.argv[2],
+                            sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State, City).join(City).filter(State.id == City.state_id).order_by(City.id).all()
+    states = session.query(State, City).join(City).filter(
+            State.id == City.state_id).order_by(City.id).all()
+
     for state, city in states:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
