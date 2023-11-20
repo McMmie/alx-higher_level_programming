7eaaee59ccb7c065f39249c869c771e7c551b0f1
McMmie <tharausamuel01@gmail.com>
2023-11-20 00:46:56 +0000

    fixed import and indent issue

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,8 @@ import sys
 
 if __name__ == '__main__':
     argv = sys.argv
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * from states")
     states = cur.fetchall()
