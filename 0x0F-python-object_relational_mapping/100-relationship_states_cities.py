d8b7c545ddaec0bed9534ea40a758d26b66b35e1
McMmie <tharausamuel01@gmail.com>
2023-11-25 00:29:10 +0000

    new updates on sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+creates the state 'Carlifonia' with the city 'San Francisco'
+from the database hbtn_0e_101_usa
+"""
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb//:{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2],
+                           sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.close()
