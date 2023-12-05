d00fc00f6f2b2e4f533e8037e5d716bcd49273f6
McMmie <tharausamuel01@gmail.com>
2023-12-05 08:14:31 -0800

    added a script to fetch the error codes

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)."""
+import urllib.error, urllib.request
+from sys import argv
+
+
+if __name__ == '__main__':
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as r:
+            page = r.read()
+            print(page.decode('utf8'))
+    except urllib.error.HTTPError as e:
+        print(f'Error code: {e.code}')
