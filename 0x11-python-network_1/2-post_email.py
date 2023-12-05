9910a702b903c5b6548ced735729f9ffc975c7b6
McMmie <tharausamuel01@gmail.com>
2023-12-05 07:38:43 -0800

    added post request to a script

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+import urllib.request, urllib.parse
+from sys import argv
+
+
+if __name__ == '__main__':
+    values = {'email' : argv[2]}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        r = response.read()
+        print(r.decode('utf8'))
