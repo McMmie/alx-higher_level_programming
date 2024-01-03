a489b834c11406dbad94cf6d61370c5cc2b06092
McMmie <tharausamuel01@gmail.com>
2024-01-03 21:25:14 +0000

    write a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// A script that writes a string to a file
+
+const fs = require('fs');
+if (process.argv.length !== 4) {
+	console.error('Usage: ./script.js <filename> <content>');
+	process.exit(1);
+}
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+	if (err){
+		console.error(err);
+	}
+});
