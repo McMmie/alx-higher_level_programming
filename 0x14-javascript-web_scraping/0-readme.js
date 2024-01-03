edc7959a3ca831952c6c725cd2a55145419ef704
McMmie <tharausamuel01@gmail.com>
2024-01-03 21:02:34 +0000

    a script to read files

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// a script that reads and prints the content of a file.
+
+const fs = require('fs');
+
+const filePath = process.argv[1];
+
+// Read file asynchronously
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data);
+        // Now you can use 'data' as the content of the file
+    }
+});
