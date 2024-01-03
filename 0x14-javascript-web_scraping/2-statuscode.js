4dad1d1990e2f7db4d7d8467bfa96d4c3184b028
McMmie <tharausamuel01@gmail.com>
2024-01-03 23:25:11 +0000

    final commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-//displays the status code of a GET request
 
-const request = require('request')
+let url = process.argv[2];
+const request = require('request');
 
-if (process.argv.length !== 3) {
-	    console.error('Usage: ./script.js <url>');
-
-request(process.argv[2])
-.on('response', function(response) {
-    console.log('code: ' + response.statusCode); // <--- Here 200
-});
-.on("error", function(err) {
-    console.log( err);
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
