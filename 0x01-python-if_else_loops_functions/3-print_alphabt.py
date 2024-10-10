7b51e6e741daad969ffa35543bd55bc32ddf34ce
McMmie <tharausamuel01@gmail.com>
2024-10-10 08:01:07 +0000

    added string format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for char in range(97, 123):
     if (char != 101) & (char != 113):
-        char = chr(char)
-        print(f"{char}", end='')
+        print("{}".format(chr(char)), end='')
