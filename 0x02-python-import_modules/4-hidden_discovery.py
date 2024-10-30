76c76a36a32dbef540706d7776b0eb385f18b9a8
McMmie <tharausamuel01@gmail.com>
2024-10-30 13:07:33 +0000

    changed import statement

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+alternative ways to import:
+
+    import importlib.util
+
+    spec = importlib.util.spec_from_file_location("hidden", "hidden_4.pyc")
+    hidden = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden)
+or:
+    hidden = __import__('hidden')
+"""
+if __name__ == '__main__':
+    import hidden_4 as hidden
+    for attributes in dir(hidden):
+        if '__' not in attributes:
+            print(attributes)
