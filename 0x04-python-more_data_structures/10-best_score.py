d01394113f07b823d8802b0ef9c9384fd9b0f4d3
McMmie <tharausamuel01@gmail.com>
2024-11-05 09:27:57 +0000

    refactored code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,15 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    else:
-        cpy = a_dictionary.copy()
-        a_list = []
-        for key in cpy:
-            a_list.append(cpy[key])
-        top = a_list[0]
-        for i in a_list:
-            if i > top:
-                top = i
-        for keys in a_dictionary:
-            if a_dictionary[keys] == top:
-                return keys
+
+    lst = []
+    for keys, value in a_dictionary.items():
+        lst.append(value)
+    return list(filter(lambda x: a_dictionary[x] == max(lst), a_dictionary))[0]
