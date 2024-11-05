174c2d64c2783252299193af20106b11816b0126
McMmie <tharausamuel01@gmail.com>
2024-11-05 00:56:41 +0000

    added an alternative method

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+
     cpy_matrix = matrix.copy()          # Alternatively: cpy_matrix = matrix[:]
 
     return [[i**2 for i in row] for row in cpy_matrix]
+
 
 # Alternatively, you can use this method just for the concept of it
 """
