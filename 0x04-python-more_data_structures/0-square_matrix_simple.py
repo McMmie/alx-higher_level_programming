278b346356c642c851347cc90f3b36c0acc8f1c8
McMmie <tharausamuel01@gmail.com>
2024-11-05 00:53:30 +0000

    added an alternative method

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    cpy_matrix = []
+    cpy_matrix = matrix.copy()          # Alternatively: cpy_matrix = matrix[:]
+
+    return [[i**2 for i in row] for row in cpy_matrix]
+
+# Alternatively, you can use this method just for the concept of it
+"""
     for row in matrix:
         squares = list(map(lambda x: x ** 2, row))
         cpy_matrix.append(squares)
 
     return cpy_matrix
+"""
