304ca3baa1d1fb79255585d11578e2409dd2d866
McMmie <tharausamuel01@gmail.com>
2024-12-10 22:51:31 +0000

    fixed return value issue

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,66 +1,66 @@
 #!/usr/bin/python3
 
-""" It creates a class that defines a
-square and creates a
-private instance attribute"""
+""" Defines a class that calculates a
+square and prints it"""
 
 
-class Square:
-    """A class that defines a private
-    instance attribute.
-    Args:
-        size: not sure what it does yet
-        """
-
+class Square():
+    """This is a class that defines a square
+    Attributes:
+        size: Private instance attribute
+    """
     def __init__(self, size=0):
-        """This is the docstring for the __init__
-        method.
-        not familiar with this new documentation
-        style.
-        Args:
-            size (int): only integers
-            """
         self.__size = size
 
-        @property
-        def size(self):
-            """ It retrieves the private instance attribute
-            Return:
-                the private instance attribute
-            """
-            return self.__size
+    @property
+    def size(self):
+        """a getter for our private instance attribute
 
-        @size.setter
-        def size(self, value):
-            """ Setter
-            Args:
-                value(int): integer
-            """
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        Returns:
+            int: returns size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter function for the private instance attribute
+
+        Args:
+            value (int): value to set
+
+        Raises:
+            TypeError: If the value provided is not an integer
+            ValueError: checks if value is greater than zero
+
+        Returns:
+            int: sets the private instance attribute
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+        return self.__size
 
     def area(self):
-        """This is a public instance method
-
+        """Public instance method that returns the current area
         Return:
-            the current square area
-            """
-        return (self.__size) ** 2
+            int: the area of a square
+        """
+        return self.__size ** 2
 
     def my_print(self):
+        """prints a square using the # character
         """
-        prints the square with character '#'
+        if self.__size == 0:
+            print("")
 
-        """
-        for i in range(self.__size):
-            for i in range(self.__size):
+        for _ in range(self.__size):
+            for _ in range(self.__size):
                 print("#", end="")
-
-            print('')
+            print("")
 
         """Alternatively,
         print("\n".join(["#" * self.__size for rows in range(self.__size)]))
